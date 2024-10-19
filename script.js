935184589c8111f5b9480f46cad7462c61dd18c0
Kxaled <ksoyfulla@gmail.com>
2024-10-19 22:53:53 +0530

    script and stly

---
+++ script.js
@@ -0,0 +1,4 @@
+function showWarning(event) {
+    event.preventDefault();
+    alert("This is a mock phishing site for educational purposes only. Be cautious of entering personal information on suspicious sites!");
+}
